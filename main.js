63fb100cc1acbb5ad17bfc40888099d751f380f3
Pedro Terra <pedrooterra@outlook.com>
2024-06-17 09:08:22 -0300

    feat: alteração para Array

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-let array = {
-    Nome:"Douglas",
-    Email:"douglas@gmail.com",
-    Idade: "33",
-}
-
-console.log("Digite seu email!")
-process.stdin.on("data", function(data){
-    let entrada_usuario = data.toString().trim()
-
-    if(entrada_usuario == array.Email){
-        console.log("Conta encontrada: \n")
-        console.log(array)
-    }else
-        console.log("Usuário não encontrado")
-})
+let array = [
+    {
+        Nome: "Douglas",
+        Email: "douglas@gmail.com",
+        Idade: 33
+    }
+    ]
+    
+    
+    console.log("Digite seu email!")
+    process.stdin.on("data", function(data){
+        let entrada_usuario = data.toString().trim()
+    
+        for(let i = 0; i < array.length; i++){
+            if(array[i].Email == entrada_usuario){
+                console.log("Usuário encontrado: \n")
+                console.log(array)
+            }else
+                console.log("Usuário não encontrado.")
+        }
+    
+    })
