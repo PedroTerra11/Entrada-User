0488599c129bf4014337434576c456c7a29edeca
Pedro Terra <pedrooterra@outlook.com>
2024-06-17 09:32:43 -0300

    feat: correção de alguns bugs e adicionado a confirmação

--- main.js
+++ main.js
@@ -4,16 +4,28 @@ let array = [
     Email: "douglas@gmail.com",
     Idade: 33,
   },
+
+  {
+    Nome: "Flavia",
+    Email: "flavia@gmail.com",
+    Idade: 33,
+  },
 ];
 
 console.log("Digite seu email!");
 process.stdin.on("data", function (data) {
   let entrada_usuario = data.toString().trim();
+  let confirmacao = false;
 
   for (let i = 0; i < array.length; i++) {
     if (array[i].Email == entrada_usuario) {
       console.log("Usuário encontrado: \n");
-      console.log(array);
-    } else console.log("Usuário não encontrado.");
+      console.log(array[i]);
+      confirmacao = true;
+    }
+  }
+
+  if (!confirmacao) {
+    console.log("Usuário não encontrado");
   }
 });
